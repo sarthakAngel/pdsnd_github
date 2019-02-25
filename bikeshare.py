17a2a2f48e787c01c8d2f74b1fe48cbe79fb403c
Sarthak <sarthak.patel2@aol.com>
2019-02-25 01:33:42 -0600

    docs: Add comment for data files

--- bikeshare.py
+++ bikeshare.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime
 
+# bikeshare data files for three different cities
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
