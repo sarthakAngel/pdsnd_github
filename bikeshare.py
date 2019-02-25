b28e62f281d83cf72e7161c56b047f72655289dd
Sarthak <sarthak.patel2@aol.com>
2019-02-25 01:39:35 -0600

    docs: Remove hint for using while loop

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-#get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+#get user input for city (chicago, new york city, washington). 
     error = 1 #Flag to keep asking if somthing is wrong
     city = ""
     while (error == 1):
